f87a520879be0e36fa8a420a8438f6f095cf7b30
raymond <raymondezra276@gmail.com>
2023-07-31 12:15:40 +0300

    update

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
 mongoose
-  .connect(process.env.MONGO_DATABASE_URI, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
